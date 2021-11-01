63be3f6ce6fc24211ec24cf76eb86ab564c75ae2
Hasnaa Ayman <hassnaaayman16@gmail.com>
2021-11-01 22:57:48 +0200

    removed react query and finish the repos listing

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "antd/dist/antd.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Repositories/index.tsx
+++ src/pages/Repositories/index.tsx
@@ -1,24 +1,92 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
 import Layout from "../../common/Layout";
 import { axiosApiInstance } from "../../infrastructure/utils/api";
+import { List, Avatar, Skeleton, Divider } from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Repositories = () => {
-  const [page, SetPage] = useState<number>(1);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<any>([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
 
-  const { data, isLoading } = useQuery(
-    ["repos", page],
-    () =>
-      axiosApiInstance.get(
-        `search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`
-      ),
-    {
-      keepPreviousData: true,
-      enabled: false,
+  const loadMoreData = () => {
+    if (loading) {
+      return;
     }
-  );
+    setLoading(true);
+    setPage(page + 1);
+    axiosApiInstance
+      .get(
+        `search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}&per_page=100`
+      )
+      .then((res) => {
+        console.log({ res });
 
-  return <Layout></Layout>;
+        setData([...data, ...res.data.items]);
+        setTotal(res.data.total_count);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    loadMoreData();
+  }, []);
+
+  return (
+    <Layout>
+      <div
+        id="scrollableDiv"
+        style={{
+          height: "90%",
+          overflow: "auto",
+          padding: "0 16px",
+          border: "1px solid rgba(140, 140, 140, 0.35)",
+        }}
+      >
+        <InfiniteScroll
+          dataLength={data.length}
+          next={loadMoreData}
+          hasMore={data.length < total}
+          loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+          endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+          scrollableTarget="scrollableDiv"
+        >
+          <List
+            dataSource={data}
+            rowKey="id"
+            loading={loading}
+            size="large"
+            renderItem={(item: {
+              id: string;
+              name: string;
+              description: string;
+              owner: { avatar_url: string };
+              stargazers_count: number;
+              open_issues_count: number;
+            }) => {
+              return (
+                <List.Item key={item.id}>
+                  <List.Item.Meta
+                    avatar={<Avatar size={100} src={item.owner.avatar_url} />}
+                    title={item.name}
+                    description={item.description}
+                  />
+                  <List.Item.Meta
+                    title={item.stargazers_count + " " + "stars"}
+                    description={item.open_issues_count + " " + "issues"}
+                  />
+                </List.Item>
+              );
+            }}
+          />
+        </InfiniteScroll>
+      </div>
+    </Layout>
+  );
 };
 
 export default Repositories;
